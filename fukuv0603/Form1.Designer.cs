b3074997b8e1a64f33f464c5477b5d2c570da688
A601_03 <tnk.dat.class@gmail.com>
2021-06-03 10:03:05 +0900

    omake

--- fukuv0603/Form1.Designer.cs
+++ fukuv0603/Form1.Designer.cs
@@ -73,12 +73,13 @@ namespace fukuv0603
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(596, 366);
+            this.button1.Location = new System.Drawing.Point(591, 365);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 3;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -88,6 +89,7 @@ namespace fukuv0603
             this.button2.TabIndex = 4;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -97,6 +99,7 @@ namespace fukuv0603
             this.button3.TabIndex = 5;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -106,6 +109,7 @@ namespace fukuv0603
             this.button4.TabIndex = 6;
             this.button4.Text = "button4";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
